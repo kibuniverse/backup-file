ebbf176e518fe607527bf02fa6beac437eac1039
kibuniverse <3050540005@qq.com>
2022-07-18 13:12:13 +0800

    update hammer

--- init.lua
+++ init.lua
@@ -1,9 +1,6 @@
--- Hammerspoon
+      
 -- -------------------------------
--- Watcher for changes of init.lua.    
-
-
-
+-- Watcher for changes of init.lua
 -- -------------------------------
 function reloadConfig(files)
   doReload = false
@@ -51,15 +48,15 @@ for i,key in ipairs(hyperBindings) do
 end
 
 pressedF19 = function()
- k.triggered = false
- k:enter()
+	k.triggered = false
+	k:enter()
 end
 
 releasedF19 = function()
- k:exit()
- if not k.triggered then
-   hs.eventtap.keyStroke({}, 'F18')
- end
+	k:exit()
+	if not k.triggered then
+  	hs.eventtap.keyStroke({}, 'F18')
+	end
 end
 
 f18 = hs.hotkey.bind({}, 'F19', pressedF19, releasedF19)
@@ -74,40 +71,40 @@ hs.window.animationDuration = 0
 -- Get list of screens and refresh that list whenever screens are plugged or unplugged:
 local screens = hs.screen.allScreens()
 local screenwatcher = hs.screen.watcher.new(function()
- screens = hs.screen.allScreens()
+	screens = hs.screen.allScreens()
 end)
 screenwatcher:start()
 
 -- init grid
-hs.grid.MARGINX  = 0
-hs.grid.MARGINY  = 0
-hs.grid.GRIDWIDTH  = 7
-hs.grid.GRIDHEIGHT  = 3
+hs.grid.MARGINX 	= 0
+hs.grid.MARGINY 	= 0
+hs.grid.GRIDWIDTH 	= 7
+hs.grid.GRIDHEIGHT 	= 3
 
 -- Resize window for chunk of screen.
 -- For x and y: use 0 to expand fully in that dimension, 0.5 to expand halfway
 -- For w and h: use 1 for full, 0.5 for half
 function push(x, y, w, h)
- local win = hs.window.focusedWindow()
- local f = win:frame()
- local screen = win:screen()
- local max = screen:frame()
+	local win = hs.window.focusedWindow()
+	local f = win:frame()
+	local screen = win:screen()
+	local max = screen:frame()
 
- f.x = max.x + (max.w*x)
- f.y = max.y + (max.h*y)
- f.w = max.w*w
- f.h = max.h*h
- win:setFrame(f)
+	f.x = max.x + (max.w*x)
+	f.y = max.y + (max.h*y)
+	f.w = max.w*w
+	f.h = max.h*h
+	win:setFrame(f)
 end
 
 -- Show grid
 hs.hotkey.bind(pushkey, 'g', hs.grid.show)
 
 -- Push to screen edge
-hs.hotkey.bind(pushkey,"h", function() push(0,0,0.5,1) end)   -- left side
-hs.hotkey.bind(pushkey,"l", function() push(0.5,0,0.5,1) end) -- right side
-hs.hotkey.bind(pushkey,"j", function() push(0,0,1,0.5) end)   -- top half
-hs.hotkey.bind(pushkey,"k", function() push(0,0.5,1,0.5) end) -- bottom half
+hs.hotkey.bind(pushkey,"h", function() push(0,0,0.5,1) end) 		-- left side
+hs.hotkey.bind(pushkey,"l", function() push(0.5,0,0.5,1) end)	-- right side
+hs.hotkey.bind(pushkey,"j", function()	push(0,0,1,0.5) end) 		-- top half
+hs.hotkey.bind(pushkey,"k", function()	push(0,0.5,1,0.5) end)	-- bottom half
 
 -- Center window with some room to see the desktop
 hs.hotkey.bind(pushkey, "m", function() push(0.05,0.05,0.9,0.9) end)
@@ -143,26 +140,26 @@ end
 
 
 -- Applications, toggle visibility
-hs.hotkey.bind(alt, 'i', function() toggle_app('Iterm') end)
-hs.hotkey.bind(alt, 'v', function() toggle_app('Visual Studio code') end)
-hs.hotkey.bind(alt, 'g', function() toggle_app('Google Chrome') end)
-hs.hotkey.bind(alt, 'c', function() toggle_app('Charles') end)
-hs.hotkey.bind(alt, 'd', function() toggle_app('DingTalk') end)
-hs.hotkey.bind(alt, 'n', function() toggle_app('NeteaseMusic') end)
--- hs.hotkey.bind(alt, 'e', function() toggle_app('Zeplin') end)
--- hs.hotkey.bind(alt, 'l', function() toggle_app('Lark') end)
--- hs.hotkey.bind(alt, 'n', function() toggle_app('Cmd Markdown') end)
+hs.hotkey.bind(alt, '1', function() toggle_app('Warp') end)
+hs.hotkey.bind(alt, '2', function() toggle_app('Visual Studio code') end)
+hs.hotkey.bind(alt, 's', function() toggle_app('Google Chrome') end)
+hs.hotkey.bind(alt, 'x', function() toggle_app('qq') end)
+hs.hotkey.bind(alt, 'i', function() toggle_app('idea') end)
+hs.hotkey.bind(alt, 'd', function() toggle_app('music') end)
+hs.hotkey.bind(alt, 'a', function() toggle_app('Lark') end)
+hs.hotkey.bind(alt, 't', function() toggle_app('Typora') end)
+hs.hotkey.bind(alt, 'n', function() toggle_app('Cmd Markdown') end)
 hs.hotkey.bind(alt, 'b', function() toggle_app('Notes') end)
 hs.hotkey.bind(alt, 'f', function() toggle_app('Finder') end)
 hs.hotkey.bind(alt, 'w', function() toggle_app('Wechat') end)
-hs.hotkey.bind(alt, 'm', function() toggle_app('QQMusic') end)
-hs.hotkey.bind(alt, 'q', function() toggle_app('QQ') end)
---hs.hotkey.bind(alt, 'l', function() toggle_app('Seal') end)
+hs.hotkey.bind(alt, 'r', function() toggle_app('RunJS') end)
+hs.hotkey.bind(alt, 'p', function() toggle_app('WPS') end)
+hs.hotkey.bind(alt, 'l', function() toggle_app('Seal') end)
 -- hs.hotkey.bind(alt, 'x', function() toggle_app('Xcode') end)
 -- hs.hotkey.bind(alt, 'w', function() toggle_app('Wunderlist') end)
---hs.hotkey.bind(alt, '3', function() toggle_app('Microsoft Edge') end)
+hs.hotkey.bind(alt, '3', function() toggle_app('Microsoft Edge') end)
 -- hs.hotkey.bind(alt, 's', function() toggle_app('Simulator') end)
---hs.hotkey.bind(alt, 'm', function() toggle_app('Simulator') end)
+hs.hotkey.bind(alt, 'm', function() toggle_app('Simulator') end)
 
 
 -- -----
@@ -179,10 +176,12 @@ hs.hotkey.bind(hyper, 'h', function() hs.eventtap.keyStroke({}, 'left') end)
 -- Help. Lists shortcuts, etc.
 -- The terrible spacing looks fine when the alert is actually displayed
 hs.hotkey.bind(hyper, "Q", function()
- helpstr = [[Hyper     ⌘⌥⌃⇧
+	helpstr = [[Hyper					⌘⌥⌃⇧
 Pushkey             ⌘⌥⌃q
-Push     ⌘⌃
-Grid      Pushkey-G
+Push					⌘⌃
+Grid						Pushkey-G
 ]]
- hs.alert.show(helpstr)
+	hs.alert.show(helpstr)
 end)
+
+    
